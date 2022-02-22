98a3b60c590583c8c3159b33d5e6dd95f3353abe
tanush-linux <tanush.mohina@gmail.com>
2022-02-22 17:05:19 +0530

    correction

--- Arrays/modify-array-with-function.cpp
+++ Arrays/modify-array-with-function.cpp
@@ -1,14 +1,14 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-void func(int  arr[])
+void func(int  * arr)
 {
-  (arr)[4] = 10;
+  (*arr)[4] = 10;
 }
 int main() {
   int arr[5] = {1,23,42,5,62};
   cout<<arr[4]<<endl;
-  func(arr);
+  func(&arr);
   cout<<arr[4]<<endl;
   return 0;
 
